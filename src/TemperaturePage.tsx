bfbc09c67b1afc4420c89b2b6759d0fd30361950
Sára Vogel <sari.vogel.94@gmail.com>
2021-08-25 22:29:02 +0200

    Extract TemperatureTable to its own component

--- src/TemperaturePage.tsx
+++ src/TemperaturePage.tsx
@@ -1,24 +1,11 @@
 import { useState } from 'react'
-import { makeStyles } from '@material-ui/core/styles'
-import Table from '@material-ui/core/Table'
-import TableBody from '@material-ui/core/TableBody'
-import TableCell from '@material-ui/core/TableCell'
 import TableContainer from '@material-ui/core/TableContainer'
-import TableHead from '@material-ui/core/TableHead'
-import TableRow from '@material-ui/core/TableRow'
 import Paper from '@material-ui/core/Paper'
 import useTemperatureRows from './Temperatures/query'
-import { Temperature } from './Temperatures/Temperature'
 import GeoForm from './Forms/GeoForm'
-
-const useStyles = makeStyles({
-    table: {
-        minWidth: 650,
-    },
-})
+import TemperatureTable from './TemperatureTable'
 
 export default function TemperaturePage() {
-    const classes = useStyles()
 
     const [coordinates, setCoordinates] = useState({lat:0, lon:0})
 
@@ -33,38 +20,7 @@ export default function TemperaturePage() {
         <TableContainer component={Paper}>
             <GeoForm handleSubmit={updateCoordinates} />
             <h1>Weather report of last 4 days</h1>
-            <Table className={classes.table} aria-label="simple table">
-                <TableHead>
-                    <TableRow>
-                        <TableCell>Göteborg</TableCell>
-                        <TableCell align="right">Mean (°C)</TableCell>
-                        <TableCell align="right">Median (°C)</TableCell>
-                        <TableCell align="right">Minimum (°C)</TableCell>
-                        <TableCell align="right">Maximum (°C)</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {rows?.map((row: Temperature) => (
-                        <TableRow key={row.date}>
-                            <TableCell component="th" scope="row">
-                                {row.date}
-                            </TableCell>
-                            <TableCell align="right">
-                                {row.meanTemperature}
-                            </TableCell>
-                            <TableCell align="right">
-                                {row.medianTemperature}
-                            </TableCell>
-                            <TableCell align="right">
-                                {row.minTemperature}
-                            </TableCell>
-                            <TableCell align="right">
-                                {row.maxTemperature}
-                            </TableCell>
-                        </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
+            <TemperatureTable rows={rows} />
         </TableContainer>
     )
 
---
+++ src/TemperatureTable.tsx
@@ -0,0 +1,57 @@
+
+import { makeStyles } from '@material-ui/core/styles'
+import Table from '@material-ui/core/Table'
+import TableBody from '@material-ui/core/TableBody'
+import TableCell from '@material-ui/core/TableCell'
+import TableHead from '@material-ui/core/TableHead'
+import TableRow from '@material-ui/core/TableRow'
+import { Temperature } from './Temperatures/Temperature'
+
+const useStyles = makeStyles({
+    table: {
+        minWidth: 650,
+    },
+})
+
+type Props = {
+    rows: Temperature[] | undefined
+}
+
+export default function TemperaturePage(props: Props) {
+    const classes = useStyles()
+
+    return (
+        <Table className={classes.table} aria-label="simple table">
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Göteborg</TableCell>
+                        <TableCell align="right">Mean (°C)</TableCell>
+                        <TableCell align="right">Median (°C)</TableCell>
+                        <TableCell align="right">Minimum (°C)</TableCell>
+                        <TableCell align="right">Maximum (°C)</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {props.rows?.map((row: Temperature) => (
+                        <TableRow key={row.date}>
+                            <TableCell component="th" scope="row">
+                                {row.date}
+                            </TableCell>
+                            <TableCell align="right">
+                                {row.meanTemperature}
+                            </TableCell>
+                            <TableCell align="right">
+                                {row.medianTemperature}
+                            </TableCell>
+                            <TableCell align="right">
+                                {row.minTemperature}
+                            </TableCell>
+                            <TableCell align="right">
+                                {row.maxTemperature}
+                            </TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+    )
+}
