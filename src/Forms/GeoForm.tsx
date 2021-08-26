843b11954d3ba4c6da64b2920431dfc672e9439b
Sára Vogel <sari.vogel.94@gmail.com>
2021-08-26 21:59:00 +0200

    Add form field validation

--- src/Forms/GeoForm.tsx
+++ src/Forms/GeoForm.tsx
@@ -3,7 +3,17 @@ import {
     Form,
     Field,
 } from 'formik'
+import * as Yup from 'yup';
 import { Coordinates } from '../Types/Coordinates'
+
+const GeoSchema = Yup.object().shape({
+    lat: Yup.number()
+            .min(-90, 'Latitude must be minimum -90')
+            .max(90, 'Latitude must be maximum 90'),
+    lon: Yup.number()
+            .min(-180, 'Longitude must be minimum -180')
+            .max(180, 'Longitude must be maximum 180'),
+  });
 
 type Props = {
     handleSubmit: (lat: number, lon: number) => void
@@ -23,12 +33,19 @@ export default function GeoForm(props: Props) {
     return(
         <Formik
             initialValues={initialCoordinates}
+            validationSchema={GeoSchema}
             onSubmit={updateCoordinates}
         >
-            {({ isSubmitting }) => (
+            {({ isSubmitting, errors, touched }) => (
                 <Form>
-                    <Field type="number" name="lat" />
-                    <Field type="number" name="lon" />
+                    <Field type="number" step=".0001" name="lat" />
+                    {errors.lat && touched.lat ? (
+                        <div>{errors.lat}</div>
+                    ) : null}
+                    <Field type="number" step=".0001" name="lon" />
+                    {errors.lon && touched.lon ? (
+                        <div>{errors.lon}</div>
+                    ) : null}
                     <button type="submit" disabled={isSubmitting}>
                         Submit
                     </button>
