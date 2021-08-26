94abb329111db5be88c01bd762ef2c6ebf4312d1
Sára Vogel <sari.vogel.94@gmail.com>
2021-08-26 22:38:54 +0200

    Change URL to history

--- src/Temperatures/query.ts
+++ src/Temperatures/query.ts
@@ -8,7 +8,7 @@ axios.defaults.baseURL = 'http://localhost:8000'
 export default function useTemperatureRows(coordinates: Coordinates): UseQueryResult<Temperature[]> {
 	return useQuery<Coordinates, unknown, Temperature[]>(['temperatures', coordinates], () =>
     axios
-        .get(`/?lat=${coordinates.lat}&lon=${coordinates.lon}`)
+        .get(`/history/?lat=${coordinates.lat}&lon=${coordinates.lon}`)
         .then(res => res.data)
     );
 }
